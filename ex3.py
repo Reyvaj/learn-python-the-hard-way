941d136f1f9afe58cd6b180584059085b1946221
Reyvaj <javier.ortiz989@gmail.com>
2016-12-07 14:37:29 -0800

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,41 @@
+# prints a statement.
+print "I will now count my chickens:"
+
+# prints the word "Hens" followed by a division and a sum.
+print "Hens", 25.0 + 30 / 6
+
+# prints the word "Roosters" followed by the result of the operations.
+print "Roosters", 100.0 - 25 * 3 % 4
+
+# prints a statement.
+print "Now I will count the eggs:"
+
+# prints the result of the operation.
+print 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# prints a question.
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Judges a comparison between two operations.
+print 3.0 + 2 < 5 - 7
+
+# prints a question followed by the result of an operation.
+print "What is 3 + 2?", 3.0 + 2
+
+# prints a question followed by the result of an operation.
+print "What is 5 - 7?", 5.0 - 7
+
+# prints a statement.
+print "Oh, that's why it's False."
+
+# prints a statement.
+print "How about some more."
+
+# prints a question followed by the boolean result of the comparison.
+print "Is it greater?", 5.0 > -2
+
+# prints a question followed by the boolean result of the comparison.
+print "Is it greater or equal?", 5.0 >= -2
+
+# prints a question followed by the boolean result of the comparison.
+print "Is it less or equal?", 5.0 <= -2
